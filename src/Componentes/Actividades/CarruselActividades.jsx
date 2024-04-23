a01db51914b85658c8796e285050f4f4e748ae80
JesusArm98 <141459949+JesArm98@users.noreply.github.com>
2024-04-22 17:31:27 -0700

    aber2

--- src/Componentes/Actividades/CarruselActividades.jsx
+++ src/Componentes/Actividades/CarruselActividades.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { data } from "../../assets/data";
 import "./CarruselActividades.css";
 
@@ -6,9 +6,9 @@ const CarruselActividades = () => {
   const listRef = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentTextos, setCurrentTextos] = useState({});
-  const [showMore, setShowMore] = useState(false); // Estado para mostrar el texto adicional
-  const [activeButton, setActiveButton] = useState(0); // Estado para mantener el botón activo
-  const [startTouch, setStartTouch] = useState({ x: null, y: null }); // Estado para el inicio del toque
+  const [showMore, setShowMore] = useState(false);
+  const [activeButton, setActiveButton] = useState(0);
+  const [startTouch, setStartTouch] = useState({ x: null, y: null });
 
   useEffect(() => {
     const listNode = listRef.current;
@@ -20,24 +20,21 @@ const CarruselActividades = () => {
       });
     }
 
-    // Actualizar los textos correspondientes a la imagen actual
     setCurrentTextos(data[currentIndex].textos);
   }, [currentIndex]);
 
   const handleButtonClick = (idx) => {
-    setActiveButton(idx); // Establecer el botón activo
-    setCurrentIndex(idx); // Ir al slide correspondiente al botón
-    setShowMore(false); // Reiniciar el estado para ocultar el texto adicional
+    setActiveButton(idx);
+    setCurrentIndex(idx);
+    setShowMore(false);
   };
 
   const handleShowMore = () => {
-    setShowMore(!showMore); // Alternar el estado para mostrar u ocultar el texto adicional
+    setShowMore(!showMore);
     const textosCard = document.querySelector(".textos-card");
     if (!showMore) {
-      // Si se muestra más, agrega la clase de opacidad alterna
       textosCard.classList.add("opacidad-alternativa");
     } else {
-      // Si se muestra menos, quita la clase de opacidad alterna
       textosCard.classList.remove("opacidad-alternativa");
     }
   };
@@ -60,21 +57,13 @@ const CarruselActividades = () => {
     const deltaY = touch.clientY - startTouch.y;
 
     if (Math.abs(deltaX) > Math.abs(deltaY)) {
-      // movimiento horizontal
-      if (deltaX > 0) {
-        // Swipe hacia la derecha
-        if (currentIndex > 0) {
-          setCurrentIndex(currentIndex - 1);
-        }
-      } else {
-        // Swipe hacia la izquierda
-        if (currentIndex < data.length - 1) {
-          setCurrentIndex(currentIndex + 1);
-        }
+      if (deltaX > 0 && currentIndex > 0) {
+        setCurrentIndex(currentIndex - 1);
+      } else if (deltaX < 0 && currentIndex < data.length - 1) {
+        setCurrentIndex(currentIndex + 1);
       }
     }
 
-    // Resetear después del movimiento para prepararse para el siguiente swipe
     setStartTouch({ x: null, y: null });
   };
 
@@ -102,16 +91,13 @@ const CarruselActividades = () => {
           </ul>
         </div>
         <div className="textos-card">
-          {/* Renderiza original-text y hola123 cuando showMore es false */}
           {!showMore && (
             <div>
               <div className="original-text">{currentTextos.originalText}</div>
             </div>
           )}
-          {/* Mostrar el nuevo texto solo cuando se presione el botón */}
           {showMore && (
             <div className="nuevo-texto">
-              {/* Renderizar la lista de servicios */}
               <div className={`textoImagenText${showMore ? "" : "hidden"}`}>
                 {currentTextos.textoImagenText}
               </div>
