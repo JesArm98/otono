747bbd8bb4334cf7fd81afccc3ae1be8a8907f5a
JesusArm98 <141459949+JesArm98@users.noreply.github.com>
2024-04-22 17:51:46 -0700

    aber2

--- src/Componentes/Actividades/CarruselActividades.jsx
+++ src/Componentes/Actividades/CarruselActividades.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { data } from "../../assets/data";
 import "./CarruselActividades.css";
 
@@ -6,9 +6,10 @@ const CarruselActividades = () => {
   const listRef = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentTextos, setCurrentTextos] = useState({});
-  const [showMore, setShowMore] = useState(false); // Estado para mostrar el texto adicional
-  const [activeButton, setActiveButton] = useState(0); // Estado para mantener el botón activo
-  const [startTouch, setStartTouch] = useState({ x: null, y: null }); // Estado para el inicio del toque
+  const [showMore, setShowMore] = useState(false);
+  const [activeButton, setActiveButton] = useState(0);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
 
   useEffect(() => {
     const listNode = listRef.current;
@@ -17,69 +18,60 @@ const CarruselActividades = () => {
     if (imgNode) {
       imgNode.scrollIntoView({
         behavior: "smooth",
+        block: "nearest",
+        inline: "start",
       });
     }
 
-    // Actualizar los textos correspondientes a la imagen actual
     setCurrentTextos(data[currentIndex].textos);
   }, [currentIndex]);
 
   const handleButtonClick = (idx) => {
-    setActiveButton(idx); // Establecer el botón activo
-    setCurrentIndex(idx); // Ir al slide correspondiente al botón
-    setShowMore(false); // Reiniciar el estado para ocultar el texto adicional
+    setActiveButton(idx);
+    setCurrentIndex(idx);
+    setShowMore(false);
   };
 
   const handleShowMore = () => {
-    setShowMore(!showMore); // Alternar el estado para mostrar u ocultar el texto adicional
+    setShowMore(!showMore);
     const textosCard = document.querySelector(".textos-card");
     if (!showMore) {
-      // Si se muestra más, agrega la clase de opacidad alterna
       textosCard.classList.add("opacidad-alternativa");
     } else {
-      // Si se muestra menos, quita la clase de opacidad alterna
       textosCard.classList.remove("opacidad-alternativa");
     }
   };
 
   const handleTouchStart = (e) => {
-    const touch = e.touches[0];
-    setStartTouch({
-      x: touch.clientX,
-      y: touch.clientY,
-    });
+    setTouchStartX(e.touches[0].clientX);
   };
 
   const handleTouchMove = (e) => {
-    if (!startTouch.x || !startTouch.y) {
-      return;
+    setTouchEndX(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStartX - touchEndX > 50) {
+      const nextIndex = currentIndex === data.length - 1 ? 0 : currentIndex + 1;
+      setCurrentIndex(nextIndex);
+      setActiveButton(nextIndex);
+    } else if (touchEndX - touchStartX > 50) {
+      const prevIndex = currentIndex === 0 ? data.length - 1 : currentIndex - 1;
+      setCurrentIndex(prevIndex);
+      setActiveButton(prevIndex);
     }
-
-    const touch = e.touches[0];
-    const deltaX = touch.clientX - startTouch.x;
-    const deltaY = touch.clientY - startTouch.y;
-
-    if (Math.abs(deltaX) > Math.abs(deltaY)) {
-      // movimiento horizontal
-      if (deltaX > 0) {
-        // Swipe hacia la derecha
-        if (currentIndex > 0) {
-          setCurrentIndex(currentIndex - 1);
-        }
-      } else {
-        // Swipe hacia la izquierda
-        if (currentIndex < data.length - 1) {
-          setCurrentIndex(currentIndex + 1);
-        }
-      }
-    }
-
-    // Resetear después del movimiento para prepararse para el siguiente swipe
-    setStartTouch({ x: null, y: null });
+    setTouchStartX(0);
+    setTouchEndX(0);
   };
 
   return (
-    <div id="actividades2" className="main-container">
+    <div
+      id="actividades2"
+      className="main-container"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <h1 className="tituloEstancias">Actividades</h1>
       <p className="subtituloEstancias">
         Descubre cómo enriquecemos la vida de nuestros residentes a través de
@@ -87,11 +79,7 @@ const CarruselActividades = () => {
         socialización y promover un envejecimiento activo y pleno.
       </p>
 
-      <div
-        className="slider-container"
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-      >
+      <div className="slider-container">
         <div className="container-images">
           <ul ref={listRef}>
             {data.map((item, index) => (
@@ -102,17 +90,14 @@ const CarruselActividades = () => {
           </ul>
         </div>
         <div className="textos-card">
-          {/* Renderiza original-text y hola123 cuando showMore es false */}
           {!showMore && (
             <div>
               <div className="original-text">{currentTextos.originalText}</div>
             </div>
           )}
-          {/* Mostrar el nuevo texto solo cuando se presione el botón */}
           {showMore && (
             <div className="nuevo-texto">
-              {/* Renderizar la lista de servicios */}
-              <div className={`textoImagenText${showMore ? "" : "hidden"}`}>
+              <div className={`textoImagenText ${showMore ? "" : "hidden"}`}>
                 {currentTextos.textoImagenText}
               </div>
             </div>
